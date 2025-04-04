949e63b39c362dfb0675131f6e50bd554b2c9d6a
Nils <152011377+VELIKSONNN@users.noreply.github.com>
2025-04-04 17:01:53 +0200

    remplacement test

--- MetroGraphe/MetroGraphe/Graphe.cs
+++ MetroGraphe/MetroGraphe/Graphe.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 /*<<<<<<< Updated upstream
 <<<<<<< Updated upstream
 ﻿
@@ -5,6 +6,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+=======
+﻿using livinparis_dufourmantelle_veyrie;
+>>>>>>> Stashed changes
 =======
 ﻿using livinparis_dufourmantelle_veyrie;
 >>>>>>> Stashed changes
@@ -26,11 +30,17 @@ namespace livinparis_dufourmantelle_veyrie
 
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
             // Calcul de la taille maximale pour la liste d’adjacence
             int maxId = Convert.ToInt32(Noeuds.Max(n => Convert.ToInt32(n.ID))) + 1;
             ListeAdjacente = new List<Lien<T>>[maxId];
 
             // Initialisation de chaque sous-liste
+=======
+            int maxId = Convert.ToInt32(Noeuds.Max(n => Convert.ToInt32(n.ID))) + 1;
+            ListeAdjacente = new List<Lien<T>>[maxId];
+
+>>>>>>> Stashed changes
 =======
             int maxId = Convert.ToInt32(Noeuds.Max(n => Convert.ToInt32(n.ID))) + 1;
             ListeAdjacente = new List<Lien<T>>[maxId];
@@ -48,7 +58,10 @@ namespace livinparis_dufourmantelle_veyrie
 
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
             // Peuplement de la structure d’adjacence
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
@@ -67,6 +80,7 @@ namespace livinparis_dufourmantelle_veyrie
             ListeAdjacente[index].Add(lien);
         }
 
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
         /// <summary>
@@ -130,6 +144,12 @@ namespace livinparis_dufourmantelle_veyrie
         public List<Noeud<T>> Dijkstra(Noeud<T> depart, Noeud<T> arrivee)
         {
 >>>>>>> Stashed changes
+=======
+
+
+        public List<Noeud<T>> Dijkstra(Noeud<T> depart, Noeud<T> arrivee)
+        {
+>>>>>>> Stashed changes
             var distances = new Dictionary<T, double>();
             var precedent = new Dictionary<T, Noeud<T>>();
             var nonVisites = new List<Noeud<T>>(Noeuds);
@@ -160,6 +180,9 @@ namespace livinparis_dufourmantelle_veyrie
                         distances[voisin.ID] = tentative;
                         precedent[voisin.ID] = noeudActuel;
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
@@ -169,6 +192,7 @@ namespace livinparis_dufourmantelle_veyrie
 
             // Reconstruction du chemin
             var chemin = new List<Noeud<T>>();
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
             int actuel = Convert.ToInt32(arrivee.ID);
@@ -221,6 +245,22 @@ namespace livinparis_dufourmantelle_veyrie
         }
 
 >>>>>>> Stashed changes
+=======
+            var courant = arrivee;
+
+            while (courant != null && precedent.ContainsKey(courant.ID))
+            {
+                chemin.Insert(0, courant);
+                courant = precedent[courant.ID];
+            }
+
+            if (courant != null && courant.ID.Equals(depart.ID))
+                chemin.Insert(0, depart);
+
+            return chemin;
+        }
+
+>>>>>>> Stashed changes
         public List<Noeud<T>> BellmanFord(Noeud<T> depart, Noeud<T> arrivee)
         {
             int n = ListeAdjacente.Length;
@@ -229,7 +269,10 @@ namespace livinparis_dufourmantelle_veyrie
 
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
             // Initialisation
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
@@ -244,7 +287,11 @@ namespace livinparis_dufourmantelle_veyrie
 
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
             // Relaxation des arêtes n-1 fois
+=======
+            // Relaxation des arêtes n - 1 fois
+>>>>>>> Stashed changes
 =======
             // Relaxation des arêtes n - 1 fois
 >>>>>>> Stashed changes
@@ -280,6 +327,7 @@ namespace livinparis_dufourmantelle_veyrie
                     if (distance[u] != double.MaxValue && distance[u] + poids < distance[v])
                     {
                         throw new InvalidOperationException("Le graphe contient un cycle de poids négatif.");
+<<<<<<< Updated upstream
                     }
                 }
             }
@@ -517,3 +565,23 @@ namespace livinparis_dufourmantelle_veyrie
         }
     }
 }
+=======
+                    }
+                }
+            }
+
+            // Construction du chemin
+            List<Noeud<T>> chemin = new List<Noeud<T>>();
+            int current = Convert.ToInt32(arrivee.ID);
+            while (current != -1)
+            {
+                chemin.Insert(0, Noeuds.First(n => Convert.ToInt32(n.ID).Equals(current)));
+                current = predecesseur[current];
+            }
+
+            return chemin;
+        }
+
+    }
+}
+>>>>>>> Stashed changes
--- MetroGraphe/MetroGraphe/obj/Debug/net8.0/Rendu2livinparis_dufourmantelle_veyrie.AssemblyInfo.cs
+++ MetroGraphe/MetroGraphe/obj/Debug/net8.0/Rendu2livinparis_dufourmantelle_veyrie.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rendu2livinparis_dufourmantelle_veyrie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21b39ad1e9ccc6dfc311de574c95d9eab403e764")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+746e79fbca84f0b920235fa71b1db7ccc623b8e8")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("Rendu2livinparis_dufourmantelle_veyrie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rendu2livinparis_dufourmantelle_veyrie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
